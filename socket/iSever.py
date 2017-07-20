b6fb83b0a5186b4bc1a63bf5be1796bb0b8cbf4b
LazyWorm <641541452@qq.com>
2017-07-20 21:50:09 +0800

    First commit

--- socket/iSever.py
+++ socket/iSever.py
@@ -1,23 +1,25 @@
 #coding: utf-8
 from socket import *
 
-s = socket(AF_INET, SOCK_STREAM)
-# 第一个参数是"服务器之间网络通信", 第二个参数是"流式socket , for TCP"
-s.bind(('', 6666))
-# 将套接字绑定到地址，第一个参数是绑定本机，地二个参数是端口号6666
-s.listen(1)
-# 开始监听传入连接，参数里面的数字表示最大连接数量
-sock, addr = s.accept()
-# 接受连接并返回（conn,address）,其中conn是新的套接字对象，可以用来接收和发送数据。address是连接客户端的地址。
-print('Connect by ', addr)
-# 打印
-sock.send("Welcome!!".encode())
-# 发送信息
-text = sock.recv(1024)
-# 接受套接字的数据
-print(text)
-# 打印
-sock.close()
-# 关闭
-s.close()
-# 关闭
+
+if __name__ == "__main__":
+	# 第一个参数是"服务器之间网络通信", 第二个参数是"流式socket , for TCP"
+	s = socket(AF_INET, SOCK_STREAM)
+	# 将套接字绑定到地址，第一个参数是绑定本机，地二个参数是端口号6666
+	s.bind(('', 6666))
+	# 开始监听传入连接，参数里面的数字表示最大连接数量
+	s.listen(1)
+	# 接受连接并返回（conn,address）,其中conn是新的套接字对象，可以用来接收和发送数据。address是连接客户端的地址。
+	sock, addr = s.accept()
+	# 打印
+	print('Connect by ', addr)
+	# 发送信息
+	sock.send("Welcome!!".encode())
+	# 接受套接字的数据
+	text = sock.recv(1024)
+	# 打印
+	print(text)
+	# 关闭
+	sock.close()
+	# 关闭
+	s.close()
--- 多线程SSH/iSever.py
+++ 多线程SSH/iSever.py
@@ -1,10 +1,24 @@
 #coding: utf-8
 from socket import *
 from os import *
+from threading import Thread
 
+
+#创建一个接受服务函数
+def ThreadHandle(sock):
+	# 接受信息
+	cmd = sock.recv(1024)
+	# 判断是否为exit
+	if cmd == 'exit':
+		# 退出
+		exit()
+	# 执行上面接收到的cmd命令	
+	result = popen(cmd).read()
+	# 发送执行cmd命令的结果
+	sock.send(result)
 
 if __name__ == "__main__":
-	# 第一个参数是"服务器之间网络通信", 第二个参数是"流式socket , for TCP"
+	# 第一个参数是"服务器之间网络通信", 第二个参数是"流式socket , for TCP"	
 	s = socket(AF_INET, SOCK_STREAM)
 	# 将套接字绑定到地址，第一个参数是绑定本机，地二个参数是端口号6666
 	s.bind(('', 6666))
@@ -13,20 +27,11 @@ if __name__ == "__main__":
 	# 设置一个循环
 	while True:
 		# 接受连接并返回（conn,address）,其中conn是新的套接字对象，可以用来接收和发送数据。address是连接客户端的地址。
+		print('Connect by ', addr)
 		sock, addr = s.accept()
 		# 打印
 		print('Connect by ', addr)
-		# 循环
-		while True:
-			# 接受信息
-			cmd = sock.recv(1024)
-			# 判断是否为exit
-			if cmd == 'exit':
-				# 断开连接
-				sock.close()
-				# 跳出循环
-				break
-			# 执行上面接收到的cmd命令
-			result = popen(cmd).read()
-			# 发送执行cmd命令的结果
-			sock.send(result)
+		# 生成多线程池
+		t = Thread(target = ThreadHandle, args = (sock,))
+		# 启动多线程
+		t.start()
