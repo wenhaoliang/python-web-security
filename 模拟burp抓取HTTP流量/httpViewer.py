8b8a91ea510d2eac0a5dc1211fe5513f5dbfdcda
LazyWorm <641541452@qq.com>
2017-07-21 18:27:11 +0800

    添加模拟burp抓取HTTP流量

---
+++ 模拟burp抓取HTTP流量/httpViewer.py
@@ -0,0 +1,56 @@
+#coding: utf-8 
+
+
+import SocketServer
+import re
+from socket import * 
+
+addr = ('127.0.0.1', 8080)
+
+
+def getHost(dataPack):
+	result = re.search(r'Host:\s(.*?)\s', dataPack)
+	host = result.group(1)
+	return host
+	
+	
+class myProxy(SocketServer.BaseRequestHandler):
+
+
+	def handle(self):
+		self.httpReqst = self.request.recv(1024)
+		print(self.httpReqst)
+		self.rhost = getHost(self.httpReqst)
+		newSock = socket(AF_INET, SOCK_STREAM)
+		newSock.connect((str(self.rhost), 80))
+		newSock.send(self.httpReqst)
+		buffer = []
+		while True:
+			d = newSock.recv(1024)
+			if d:
+				buffer.append(d)
+			else:
+				break
+		self.httpReqst = ''.join(buffer)
+		self.request.sendall(self.httpReqst)
+
+		
+if __name__ == '__main__':
+	server = SocketServer.ThreadingTCPServer(addr, myProxy)
+	server.serve_forever()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
